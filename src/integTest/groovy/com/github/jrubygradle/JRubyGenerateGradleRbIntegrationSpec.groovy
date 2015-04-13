4d2b66472fc3ce40e6cff07c7a204f70f18c2ecc
Schalk W. Cronjé <ysb33r@gmail.com>
2015-04-13 22:14:54 +0100

    Proposed code for generating a ruby stub. It does not have a default task yet

--- src/integTest/groovy/com/github/jrubygradle/JRubyGenerateGradleRbIntegrationSpec.groovy
+++ src/integTest/groovy/com/github/jrubygradle/JRubyGenerateGradleRbIntegrationSpec.groovy
@@ -7,7 +7,7 @@ import spock.lang.Specification
 /**
  * @author Schalk W. Cronjé
  */
-class JRubyGenerateGradleRbSpec extends Specification {
+class JRubyGenerateGradleRbIntegrationSpec extends Specification {
 
     static final File CACHEDIR = new File( System.getProperty('TEST_CACHEDIR') ?: 'build/tmp/integrationTest/cache')
     static final File FLATREPO = new File( System.getProperty('TEST_FLATREPO') ?: 'build/tmp/integrationTest/flatRepo')
